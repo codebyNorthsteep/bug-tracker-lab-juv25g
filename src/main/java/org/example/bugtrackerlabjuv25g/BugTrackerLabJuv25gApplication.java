e34573acbde850be7787fc92e7ac2e3979693db6
Caroline Nordbrandt <caroline_nordbradt@hotmail.com>
2026-03-09 09:45:51 +0100

    format: adjust code indentation and formatting in application and test classes

--- src/main/java/org/example/bugtrackerlabjuv25g/BugTrackerLabJuv25gApplication.java
+++ src/main/java/org/example/bugtrackerlabjuv25g/BugTrackerLabJuv25gApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BugTrackerLabJuv25gApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BugTrackerLabJuv25gApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BugTrackerLabJuv25gApplication.class, args);
+    }
 
 }
--- src/test/java/org/example/bugtrackerlabjuv25g/BugTrackerLabJuv25gApplicationTests.java
+++ src/test/java/org/example/bugtrackerlabjuv25g/BugTrackerLabJuv25gApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BugTrackerLabJuv25gApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
