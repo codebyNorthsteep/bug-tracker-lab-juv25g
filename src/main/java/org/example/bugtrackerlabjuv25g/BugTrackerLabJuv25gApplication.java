db246aec9291b28c96fa1806307bd977fed3eabe
Caroline Nordbrandt <caroline_nordbradt@hotmail.com>
2026-03-09 09:25:01 +0100

    Initial commit

---
+++ src/main/java/org/example/bugtrackerlabjuv25g/BugTrackerLabJuv25gApplication.java
@@ -0,0 +1,13 @@
+package org.example.bugtrackerlabjuv25g;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugTrackerLabJuv25gApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugTrackerLabJuv25gApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/bugtrackerlabjuv25g/BugTrackerLabJuv25gApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bugtrackerlabjuv25g;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugTrackerLabJuv25gApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
