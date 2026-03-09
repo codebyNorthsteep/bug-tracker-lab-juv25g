153c120c02162ede2e8c03808e071b9bbc2234a3
danne <xsz300@gmail.com>
2026-03-09 12:55:39 +0100

    Add database communication, add and save bug from

--- src/main/java/org/example/bugtrackerlabjuv25g/BugFormController.java
+++ src/main/java/org/example/bugtrackerlabjuv25g/BugFormController.java
@@ -1,23 +1,37 @@
 package org.example.bugtrackerlabjuv25g;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
+
 @Controller
 public class BugFormController {
 
-    @GetMapping("/report")
+    @Autowired
+    BugFormService bugformService;
+
+    @GetMapping("/reports/add")
     public String showBugForm(Model model){
         model.addAttribute("bugForm", new Bug());
         return "create_view";
     }
 
-    @PostMapping("/report")
+    @PostMapping("/reports/add")
     public String postBugForm(@ModelAttribute("bugform") Bug bugForm){
         System.out.println("Saved bug! " + bugForm.getTitle());
+        bugformService.saveReport(bugForm);
         return "showform";
+    }
+
+    @GetMapping("/reports/all")
+    public String showAllReports(Model model){
+        model.addAttribute("bugs", bugformService.getAllBugs());
+        return "buglist";
     }
 }
