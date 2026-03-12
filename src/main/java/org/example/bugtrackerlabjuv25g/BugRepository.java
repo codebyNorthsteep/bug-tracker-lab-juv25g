847e439a1255b290c6813a617af16fd85ea58c61
Caroline Nordbrandt <caroline_nordbradt@hotmail.com>
2026-03-12 09:59:40 +0100

    Add query methods to BugRepository for filtering by developer area and priority; include ordering methods

--- src/main/java/org/example/bugtrackerlabjuv25g/BugRepository.java
+++ src/main/java/org/example/bugtrackerlabjuv25g/BugRepository.java
@@ -2,5 +2,15 @@ package org.example.bugtrackerlabjuv25g;
 
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.List;
+
 public interface BugRepository extends ListCrudRepository<Bug, Long> {
+
+    List<Bug> findAllByDeveloperArea(DevelopmentArea developerArea);
+
+    List<Bug> findAllByPriority(Priority priority);
+
+    List<Bug> findAllByOrderByBugDateDesc();
+
+    List<Bug> findAllByOrderByPriorityDesc();
 }
