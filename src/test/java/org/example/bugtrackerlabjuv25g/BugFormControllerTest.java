dfb45e5d6418214db937839e0434c31aeb92f524
dabuek <xsz300@gmail.com>
2026-03-13 16:04:59 +0100

    get and post request test for bugformController bug form

---
+++ src/test/java/org/example/bugtrackerlabjuv25g/BugFormControllerTest.java
@@ -0,0 +1,61 @@
+package org.example.bugtrackerlabjuv25g;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+class BugFormControllerTest {
+    MockMvc mockMvc;
+    @Mock
+    BugFormService bugFormService;
+
+    @BeforeEach
+    void setup(){
+        this.mockMvc = MockMvcBuilders.standaloneSetup(new BugFormController(bugFormService)).build();
+    }
+
+
+    @Test
+    @DisplayName("Get request to add bug is presented with correct html, model and status ok")
+    void showBugForm() throws Exception{
+        mockMvc.perform(get("/reports/add"))
+                //Check httpresponse status
+                .andExpect(status().isOk())
+                //Check correct html file is presented
+                .andExpect(view().name("create_view"))
+                //Check model attribute is present
+                .andExpect(model().attributeExists("bugForm"));
+    }
+
+    @Test
+    @DisplayName("Post request with dummy form data should expect redirection to '/'")
+    void postBugForm() throws Exception{
+        //Because we use Model attribute we send data as a form and inputs field with .param
+        mockMvc.perform(post("/reports/add")
+                        .param("title", "testTitle")
+                        .param("description", "info")
+                        .param("priority", "LOW")
+                        .param("development", "BACKEND"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/"));
+    }
+
+
+    @Test
+    void homePage() {
+    }
+
+    @Test
+    void viewBugDetails() {
+    }
+}
